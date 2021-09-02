a866fd2bff741987e06c269806e23ecd93c2b3b3
Ganesh Pendyala <lak.gani@gmail.com>
2021-09-02 15:23:32 +0530

    Apply prettier

--- tests/ConferenceRoom.test.js
+++ tests/ConferenceRoom.test.js
@@ -1,45 +1,42 @@
 const ConferenceRoom = require("../src/ConferenceRoom");
 
-describe('ConferenceRoom', () => {
-    let room;
-    beforeEach(() => {
-        room = new ConferenceRoom("Aster");
-    })
-    describe('addAttendees', () => {
-        test('should add the provided attendees to the room', () => {
-            room.addAttendees(3);
-            room.addAttendees(5);
-            room.addAttendees(1);
-            expect(room.getAttendeesCount()).toBe(9);
-        })
+describe("ConferenceRoom", () => {
+  let room;
+  beforeEach(() => {
+    room = new ConferenceRoom("Aster");
+  });
+  describe("addAttendees", () => {
+    test("should add the provided attendees to the room", () => {
+      room.addAttendees(3);
+      room.addAttendees(5);
+      room.addAttendees(1);
+      expect(room.getAttendeesCount()).toBe(9);
+    });
 
-        test('should throw error when input is not a valid number', () => {
-            expect(() => {
-                room.addAttendees("3");
-            }).toThrowError(ConferenceRoom.InvalidCount);
-        })
-        
-    })
+    test("should throw error when input is not a valid number", () => {
+      expect(() => {
+        room.addAttendees("3");
+      }).toThrow(ConferenceRoom.InvalidCount);
+    });
+  });
 
-    describe('removeAttendees', () => {
-        test('should remove the provided attendees from the room', () => {
-            room.addAttendees(10);
-            room.removeAttendees(5);
-            expect(room.getAttendeesCount()).toBe(5);
-        })
+  describe("removeAttendees", () => {
+    test("should remove the provided attendees from the room", () => {
+      room.addAttendees(10);
+      room.removeAttendees(5);
+      expect(room.getAttendeesCount()).toBe(5);
+    });
 
-        test('should throw error when input is not a valid number', () => {
-            expect(() => {
-                room.removeAttendees("3");
-            }).toThrowError(ConferenceRoom.InvalidCount);
-        })
+    test("should throw error when input is not a valid number", () => {
+      expect(() => {
+        room.removeAttendees("3");
+      }).toThrow(ConferenceRoom.InvalidCount);
+    });
 
-        test('should throw error when tried to remove more than existing attendees', () => {
-            expect(() => {
-                room.removeAttendees(3);
-            }).toThrowError();
-        })
-        
-    })
-    
-})
+    test("should throw error when tried to remove more than existing attendees", () => {
+      expect(() => {
+        room.removeAttendees(3);
+      }).toThrow();
+    });
+  });
+});
